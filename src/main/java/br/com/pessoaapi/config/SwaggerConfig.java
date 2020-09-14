60c70bebd91174d8d1425caa2ceb6d2496fcd3cc
Rafael Willames <rafael.w.silva@hotmail.com>
2020-09-14 08:24:30 -0300

    feat: liberando swagger

--- src/main/java/br/com/pessoaapi/config/SwaggerConfig.java
+++ src/main/java/br/com/pessoaapi/config/SwaggerConfig.java
@@ -18,7 +18,7 @@ public class SwaggerConfig implements WebMvcConfigurer {
     public Docket api() { 
         return new Docket(DocumentationType.SWAGGER_2)  
           .select()                                  
-          .apis(RequestHandlerSelectors.any())              
+          .apis(RequestHandlerSelectors.basePackage("br.com.pessoaapi.controller"))              
           .paths(PathSelectors.any())                          
           .build();                                           
     }
--- src/main/java/br/com/pessoaapi/config/security/WebSecurityConfig.java
+++ src/main/java/br/com/pessoaapi/config/security/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -39,6 +40,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	    source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
 	    return source;
 	}
+	
+	@Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                                   "/configuration/ui",
+                                   "/swagger-resources/**",
+                                   "/configuration/security",
+                                   "/swagger-ui.html",
+                                   "/webjars/**");
+    }
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
